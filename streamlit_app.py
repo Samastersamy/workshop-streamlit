8c17985b6b2e292bbd6d6ba24404643fc04656e0
Samastersamy <95776956+Samastersamy@users.noreply.github.com>
2022-07-23 11:31:26 -0300

    TESTE

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,5 +19,6 @@ data_load_state.text('Loading data...done!')
 
 def load_data(nrows):
     data_load_state.text("Done! (using st.cache)")
-    st.subheader('Raw data')
+
+st.subheader('Raw data')
 st.write(data)
