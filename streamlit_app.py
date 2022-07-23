4a1de3e63e0a6fa93120fa30858f2c3be09634f0
Samastersamy <95776956+Samastersamy@users.noreply.github.com>
2022-07-23 11:27:22 -0300

    TESTE

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,7 @@ def load_data(nrows):
     data.rename(lowercase, axis='columns', inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
+
+data_load_state = st.text('Loading data...')
 data = load_data(10000)
+data_load_state.text('Loading data...done!')
