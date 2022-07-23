5cb03e5fb30eb6d3a97ce045e345c5c930994064
Samastersamy <95776956+Samastersamy@users.noreply.github.com>
2022-07-23 11:40:53 -0300

    TESTE

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,10 @@ hour_to_filter = 17
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 st.subheader(f'Map of all pickups at {hour_to_filter}:00')
 st.map(filtered_data)
+
+hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
+st.subheader('Raw data')
+st.write(data)
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
